ee335ecde88d848962a2c56bbea14641ee41f5e0
juanbarraza78 <juan.barraza7873@hotmail.com>
2022-02-03 17:14:01 -0300

    1er commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,12 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var apellido ;
+	let nombre ;
+		// var variable global y let variable local
+	nombre = prompt('Cual es tu nombre? ');
+	apellido = prompt('Cual es tu apellido? ');
+	//  prompt crea un imput para el usuario
+	alert("Tu nombre es: "+ nombre + " " + apellido );
+	//  alert(`Tu nombre es ${nombre} ${apellido}`) ; otra forma de poner lo mismo 
 }
-
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,9 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	var nombreIngresado = document.getElementById("txtIdNombre").value;
+	alert(nombreIngresado);
+	// .value me da el valor del elemento que busco
+	// document.getElementById("txtIdNombre") esto busca el elemento por id de algun objeto del HTML
 
 }
-
-
